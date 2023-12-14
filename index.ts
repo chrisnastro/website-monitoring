ce370075075da4b1e3d2b589a724c4a656775c66
chrisnastro <cnastro@icloud.com>
2023-12-13 21:00:41 -0500

    added config for api headers

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("hello world!");
+import { checkUrl } from "./src/tools/checkUrl";
+import { IRequest, urls } from "./src/tools/requests";
+
+
+
+const checkUrls = async (requests: IRequest[]) : Promise<void> => {
+    //map operates on everything in the array, maps are quicker than for loops
+   
+    await Promise.all(requests.map(async (r): Promise<void> => {
+        await checkUrl(r.url, r.config);
+
+    })
+    );
+};
+
+
+
+checkUrls(urls);
---
+++ src/tools/checkUrl.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+//export allows the function to be used anywhere in the code//
+export const checkUrl = async (url: string, config = {} ): Promise<void> => {
+    try {
+        console.time(url);
+        const response = await axios.get(url, config);
+        await response.data;
+        if (response.status === 200) {
+            console.log(url, "OK");
+        }
+        console.timeEnd(url);
+    } catch (e: any) {
+        console.log(url, e.code);
+    }
+};
