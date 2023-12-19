02153f753a1f746bc2f7bc085f63180b1939a135
chrisnastro <cnastro@icloud.com>
2023-12-18 20:07:57 -0500

    created parent and constructor

--- src/tools/checkUrl.ts
+++ src/tools/checkUrl.ts
@@ -1,14 +1,14 @@
 import axios from 'axios';
-import { Request } from './requests';
+import { URLRequest } from '../urlRequestClases/urlRequest';
 
 //export allows the function to be used anywhere in the code//
-export const checkUrl = async (request: Request): Promise<void> => {
+export const checkUrl = async (request: URLRequest): Promise<void> => {
     try {
         console.time(request.url);
         const response = await axios.get(request.url, request.config);
         const body = await response.data;
         if (response.status === 200) {
-            const compareResult = request.compare(body);
+            const compareResult = request.compareRequest(body);
             if (compareResult === true) {
                 console.log(request.url, "OK");
             } else {
